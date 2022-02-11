85ea6c9b9db2d2c55d8844cc487bc64961af599f
DESKTOP-8PN745C\grape <kkok0420@gmail.com>
2022-02-11 18:26:03 +0900

    20220211 18:25 순차정렬 기본 문제 풀이

--- src/codingTestPractice/day2/SortPractice.java
+++ src/codingTestPractice/day2/SortPractice.java
@@ -9,8 +9,8 @@ public class SortPractice {
 //		Arrays.sort(arr);
 		
 		int temp = 0;
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j < i; j++) {
+		for (int i = 0; i < arr.length - 1; i++) {			// i는 0 ~ 4번 총 5번 반복	
+			for (int j = i + 1; j < arr.length; j++) {		// j는 1 ~ 5번 총 5번 반복
 				if (arr[i] > arr[j]) {
 					temp = arr[i];
 					arr[i] = arr[j];
@@ -18,12 +18,14 @@ public class SortPractice {
 				}
 			}
 		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
-
-			if (i != arr.length - 1)
-				System.out.print(", ");
-		}
+		System.out.println(Arrays.toString(arr));
+//		
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.print(arr[i]);
+//
+//			if (i != arr.length - 1)
+//				System.out.print(", ");
+//		}
 	}
 
 }
