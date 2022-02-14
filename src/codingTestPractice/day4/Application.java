f84ccc94cd6280a953159c269e6b162b64a4af41
DESKTOP-8PN745C\grape <kkok0420@gmail.com>
2022-02-14 17:16:28 +0900

    20220214 17:16 프로그래머스 Level1 풀이

---
+++ src/codingTestPractice/day4/Application.java
@@ -0,0 +1,10 @@
+package codingTestPractice.day4;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		sol.solution(5);
+	}
+
+}
---
+++ src/codingTestPractice/day4/Solution.java
@@ -0,0 +1,16 @@
+package codingTestPractice.day4;
+
+public class Solution {
+	public String solution(int n) {
+        String answer = "";
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
