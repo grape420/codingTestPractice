c666fdb6fc3618c705e40c9cff2ee14a1be0537b
hyeonji <hyeonji@192.168.0.103>
2022-03-21 14:45:29 +0900

    20220321 14:45 프로그래머스 짝수와 홀수 풀

---
+++ src/codingTestPractice/day9/Solution.java
@@ -0,0 +1,17 @@
+package codingTestPractice.day9;
+
+public class Solution {
+
+	/*
+	 * 짝수와 홀수
+	 * 
+	 * 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+
+	 */
+	
+	public String solution(int num) {
+        String answer = "";
+        return answer;
+    }
+}
