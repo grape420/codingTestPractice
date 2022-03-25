47a7b402e79fddc177a03c53d5eac310749a537f
jonghyeon Kim <86546758+grape420@users.noreply.github.com>
2022-03-25 14:51:19 +0900

    20220325 14:51 프로그래머스 자연수 뒤집어 배열 만들기

--- src/codingTestPractice/day12/Solution2.java
+++ src/codingTestPractice/day12/Solution2.java
@@ -1,6 +1,22 @@
 class Solution {
-    public int[] solution(long n) {
-        int[] answer = {};
+    
+    /*
+     * 자연수 뒤집어 배열 만들기
+
+       자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+     */ 
+    public int[] solution(long n) {        
+        List<Integer> list = new ArrayList<>();
+
+        while(n != 0) {
+            list.add((int)(n % 10));
+            n /= 10;
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
