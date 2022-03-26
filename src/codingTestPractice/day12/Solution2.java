d574e26899cc025d9dc9faf446550018cd27c937
grape420 <grape420@naver.com>
2022-03-26 19:20:23 +0900

    20220326 19:20 프로그래머스 정수 내림차순으로 배치하기 풀이

--- src/codingTestPractice/day12/Solution2.java
+++ src/codingTestPractice/day12/Solution2.java
@@ -1,4 +1,10 @@
-class Solution {
+package codingTestPractice.day12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution2 {
     
     /*
      * 자연수 뒤집어 배열 만들기
@@ -6,17 +12,35 @@ class Solution {
        자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
      */ 
     public int[] solution(long n) {        
-        List<Integer> list = new ArrayList<>();
-
-        while(n != 0) {
-            list.add((int)(n % 10));
-            n /= 10;
-        }
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+//        List<Integer> list = new ArrayList<>();
+//
+//        while(n != 0) {
+//            list.add((int)(n % 10));
+//            n /= 10;
+//        }
+//
+//        int[] answer = new int[list.size()];
+//        for (int i = 0; i < list.size(); i++) {
+//            answer[i] = list.get(i);
+//        }
+//        return answer;
+    	
+    	String str = String.valueOf(n);
+    	StringBuilder sb = new StringBuilder(str);
+    	sb = sb.reverse();
+    	String[] ss = sb.toString().split("");
+    	
+    	int[] answer = new int[ss.length];
+    	for (int i = 0; i < ss.length; i++) {
+    		answer[i] = Integer.valueOf(ss[i]);
+    	}
+    	
+    	return answer;
     }
+    
+    public static void main(String[] args) {
+		Solution2 sol = new Solution2();
+		long n = 12345;
+		sol.solution(n);
+	}
 }
---
+++ src/codingTestPractice/day13/Solution.java
@@ -0,0 +1,62 @@
+package codingTestPractice.day13;
+
+import java.util.Arrays;
+
+public class Solution {
+	
+	/*
+	 * 정수 내림차순으로 배치하기
+	 * 
+	 * 함수 solution은 정수 n을 매개변수로 입력받습니다. 
+	 * n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 
+	 * 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+	 * 
+	 * 제한 조건
+	 * n은 1이상 8000000000 이하인 자연수입니다.
+	 */
+	
+	public long solution(long n) {
+        long answer = 0;
+        
+        String str = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(str);
+        String[] ss = sb.toString().split("");
+        
+        long[] iArr = new long[ss.length];
+        for (int i = 0; i< iArr.length; i++) {
+        	iArr[i] = Long.valueOf(ss[i]);
+        }
+        
+        
+        for (int i = 0; i < iArr.length; i++) {
+        	for (int j = i + 1; j < iArr.length; j++) {
+        		if (iArr[i] < iArr[j]) {
+        			long temp = iArr[i];
+        			iArr[i] = iArr[j];
+        			iArr[j] = temp;
+        		}
+        	}
+        }
+        
+        System.out.println(Arrays.toString(iArr));
+        
+        String s = "";
+        for (int i = 0; i < iArr.length; i++) {
+        	s +=iArr[i];
+        }
+        
+        System.out.println(s);
+        
+        answer = Long.valueOf(s);
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		long n = 118372;
+		
+		sol.solution(n);
+	}
+
+}
