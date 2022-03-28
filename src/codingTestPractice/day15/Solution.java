b956c5ec187ff61090d78bdcf6cb84b61bae0d75
grape420 <grape420@naver.com>
2022-03-28 23:13:38 +0900

    20220328 23:13 프로그래머스 약수의 합 풀이

--- src/codingTestPractice/day15/Solution.java
+++ src/codingTestPractice/day15/Solution.java
@@ -13,13 +13,16 @@ public class Solution {
 	
 	public int solution(int n) {
         int answer = 0;
+        for (int i = 1; i <= n; i++) {
+        	if (n % i == 0) answer += i;
+        }
         
         return answer;
     }
 	
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		int n = 12;
+		int n = 5;
 		
 		sol.solution(n);
 	}
