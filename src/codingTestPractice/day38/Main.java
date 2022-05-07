ab2f40ee081df526128d889f68378abb66d2f8c9
grape420 <grape420@naver.com>
2022-05-07 19:21:07 +0900

    20220507 19:20 백준 주사위 세개 풀이

---
+++ src/codingTestPractice/day38/Main.java
@@ -0,0 +1,49 @@
+package codingTestPractice.day38;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * 주사위 세개
+		 * 
+		 * 1에서부터 6까지의 눈을 가진 3개의 주사위를 던져서 다음과 같은 규칙에 따라 상금을 받는 게임이 있다. 
+
+			같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다. 
+			같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다. 
+			모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.  
+			예를 들어, 3개의 눈 3, 3, 6이 주어지면 상금은 1,000+3×100으로 계산되어 1,300원을 받게 된다. 
+			또 3개의 눈이 2, 2, 2로 주어지면 10,000+2×1,000 으로 계산되어 12,000원을 받게 된다. 
+			3개의 눈이 6, 2, 5로 주어지면 그중 가장 큰 값이 6이므로 6×100으로 계산되어 600원을 상금으로 받게 된다.
+			
+			3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("숫자 세개 입력 : ");
+		String[] str = br.readLine().split(" ");
+		
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		int c = Integer.parseInt(str[2]);
+		
+		int answer = 0;
+		
+		if (a == b && b == c && a == c) {
+			answer = 10000 + a * 1000;
+		} else if (a == b || a == c || b == c) {
+			if (a == b) answer = 1000 + a * 100;
+			else if (a == c) answer = 1000 + a * 100;
+			else answer = 1000 + b * 100;
+		} else {
+			answer = 100 * Math.max(a, Math.max(b, c));
+		}
+		System.out.println(answer);
+	}
+
+}
