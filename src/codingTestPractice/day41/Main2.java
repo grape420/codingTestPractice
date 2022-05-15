1aa4e47b14502c0043a90869d5bcbaa7e90da530
grape420 <grape420@naver.com>
2022-05-15 23:10:03 +0900

    20220515 23:09 백준 11724 연결 요소의 개수 구하기 풀이

--- src/codingTestPractice/day41/Main2.java
+++ src/codingTestPractice/day41/Main2.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main2 {
 	static boolean visited[];
-	static ArrayList<Integer>[] A;
-	
+	static ArrayList<Integer> A[];
 	public static void main(String[] args) throws IOException {
 
 		/*
