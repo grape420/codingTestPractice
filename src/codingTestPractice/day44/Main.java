36a27f81ce4c047ebf718a56da944b2cad0d7824
grape420 <grape420@naver.com>
2022-05-17 21:56:04 +0900

    20220517 21:55 프로그래머스 완주하지 못한 선수 풀이

---
+++ src/codingTestPractice/day44/Main.java
@@ -0,0 +1,42 @@
+package codingTestPractice.day44;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean visited[];
+	static ArrayList<ArrayList<Integer>> list;
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * 이것이 코딩테스트다 음료수 얼려 먹기
+		 * 
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("입력 : ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[n + 1];
+		list = new ArrayList<ArrayList<Integer>>();
+		
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			ArrayList<Integer> arrList = new ArrayList<Integer>();
+			arrList.add(Integer.parseInt(st.nextToken()));
+			
+			Integer.parseInt(st.nextToken());
+			list.add(arrList);
+		}
+		System.out.println(list);
+		
+		
+	}
+
+}
