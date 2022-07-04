c45b0c0476a357062a839832f0a1122f1fe185df
grape420 <grape420@naver.com>
2022-07-04 22:48:14 +0900

    20220704 22:48 백준 2609 최대공약수와 최소공배수

---
+++ src/codingTestPractice/BaekJoon/P2609최대공약수와최소공배수.java
@@ -0,0 +1,41 @@
+package codingTestPractice.BaekJoon;
+
+import java.util.Scanner;
+
+public class P2609최대공약수와최소공배수 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 백준 2609 최대공약수와 최소공배수
+		 * 
+		 * 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+
+			입력
+			첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+			
+			출력
+			첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+		 */
+		
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+ 
+		int d = gcd(a, b);	// 최대공약수
+ 
+		System.out.println(d);
+		System.out.println(a * b / d);
+	}
+	
+		// 최대공약수 재귀 방식
+		public static int gcd(int a, int b) {
+			if (b == 0)
+				return a;
+	            
+			// GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+			return gcd(b, a % b);
+		}
+
+}
